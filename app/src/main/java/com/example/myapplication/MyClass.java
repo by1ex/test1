6b410362fe5fc342df11cbb42866daea3138532a
BYLEX <zlobin.aleksei95@gmail.com>
2021-09-14 20:46:43 +0700

    docs

--- app/src/main/java/com/example/myapplication/MyClass.java
+++ app/src/main/java/com/example/myapplication/MyClass.java
@@ -1,6 +1,17 @@
 package com.example.myapplication;
 
+/**
+ * Главный класс с методами <b>Min</b> и <b>Max</b>.
+ * @autor Алексей Злобин
+ * @version 1.0
+ */
 public class MyClass {
+    /**
+     * Метод определения минимального числа
+     * @param a - первое число
+     * @param b - второе число
+     * @return возвращает минимальное из двух чисел
+     */
     public static int Min(int a, int b){
         return Math.min(a, b);
     }
